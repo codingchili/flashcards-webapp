54e9ea000065db3d1b4417ab89ec03bce7f26b13
Robin Duda <robin.duda@idainfront.se>
2017-08-23 21:01:31 +0200

    Add gradle wrapper

--- src/test/java/AuthenticationHandlerTest.java
+++ src/test/java/AuthenticationHandlerTest.java
@@ -1,16 +1,31 @@
+import com.codingchili.core.protocol.ResponseStatus;
+import com.codingchili.core.testing.RequestMock;
+import com.codingchili.core.testing.ResponseListener;
+import com.codingchili.flashcards.handlers.AuthenticationHandler;
+import io.vertx.core.json.JsonObject;
+import io.vertx.ext.unit.TestContext;
 import io.vertx.ext.unit.junit.VertxUnitRunner;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import static com.codingchili.core.configuration.CoreStrings.ID_PASSWORD;
+import static com.codingchili.core.configuration.CoreStrings.ID_USERNAME;
 
 /**
  * Tests for the authentication handler.
  */
 @RunWith(VertxUnitRunner.class)
 public class AuthenticationHandlerTest {
+    private AuthenticationHandler handler = new AuthenticationHandler();
 
     @Test
-    public void authenticate() {
-
+    public void authenticate(TestContext test) {
+        handle("authenticate", (data, status) -> {
+            System.out.println(data.encodePrettily());
+            test.assertEquals(status, ResponseStatus.ACCEPTED);
+        }, new JsonObject()
+            .put(ID_USERNAME, "user")
+            .put(ID_PASSWORD, "pw"));
     }
 
     @Test
@@ -43,4 +58,7 @@ public class AuthenticationHandlerTest {
 
     }
 
+    private void handle(String route, ResponseListener listener, JsonObject data) {
+        handler.handle(RequestMock.get(route, listener, data));
+    }
 }
